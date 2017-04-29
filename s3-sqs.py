58613dbacdcc37eb8dd6c8e32ca61315a7170e31
Niall McGinness <njwmcginness@gmail.com>
2017-04-29 01:41:36 -0700

    adding region to boto client

--- s3-sqs.py
+++ s3-sqs.py
@@ -1,3 +1,9 @@
 import boto3
 
-client = boto3.client('sqs')
+client = boto3.client('sqs', region_name="ca-central-1")
+
+queue = sqs.get_queue_by_name(QueueName='s3-upload')
+
+print(queue.url)
+print(queue.attributes.get('DelaySeconds'))
+
