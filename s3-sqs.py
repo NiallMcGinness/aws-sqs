b733a24b8de50416efac67a44e206accf75feea6
Niall McGinness <njwmcginness@gmail.com>
2017-04-29 01:44:11 -0700

    adding region to boto client

--- s3-sqs.py
+++ s3-sqs.py
@@ -1,6 +1,6 @@
 import boto3
 
-client = boto3.client('sqs', region_name="ca-central-1")
+sqs = boto3.client('sqs', region_name="ca-central-1")
 
 queue = sqs.get_queue_by_name(QueueName='s3-upload')
 
