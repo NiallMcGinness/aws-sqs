0c7336999a0e257f70af271a05ac3e69b0efbc32
Niall McGinness <njwmcginness@gmail.com>
2017-04-29 02:52:21 -0700

    adding region to boto client

--- s3-sqs.py
+++ s3-sqs.py
@@ -9,3 +9,18 @@ queue = sqs.get_queue_by_name(QueueName='s3-upload')
 print(queue.url)
 print(queue.attributes.get('DelaySeconds'))
 
+response = client.receive_message(
+    QueueUrl=queue.url,
+    AttributeNames=[
+        'All'|'Policy'|'VisibilityTimeout'|'MaximumMessageSize'|'MessageRetentionPeriod'|'ApproximateNumberOfMessages'|'ApproximateNumberOfMessagesNotVisible'|'CreatedTimestamp'|'LastModifiedTimestamp'|'QueueArn'|'ApproximateNumberOfMessagesDelayed'|'DelaySeconds'|'ReceiveMessageWaitTimeSeconds'|'RedrivePolicy'|'FifoQueue'|'ContentBasedDeduplication'|'KmsMasterKeyId'|'KmsDataKeyReusePeriodSeconds',
+    ],
+    MessageAttributeNames=[
+        'string',
+    ],
+    MaxNumberOfMessages=123,
+    VisibilityTimeout=123,
+    WaitTimeSeconds=123,
+    ReceiveRequestAttemptId='string'
+)
+
+print(response)
