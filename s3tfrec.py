fbf45f47593a265bd18e5b26cb3976ddef6aa628
Niall McGinness <njwmcginness@gmail.com>
2019-05-19 04:57:48 -0700

    s3 bucket boto3 downloading on cpu ec2 instance but gpu instance is throwing out of memory error even though there is enough memory free

--- s3tfrec.py
+++ s3tfrec.py
@@ -6,9 +6,9 @@ s3client = boto3.client('s3')
 
 bucket_name = "cellorgs"
 
-filename = 'cellorgstest.txt'
+filename = 'train224.tfrecords'
 
-output_filename = 'test-dowload.txt'
+output_filename = 'train224.tfrecords'
 #s3.download_file(bucket,'cellorgstest.txt','test-dowload.txt')
 
 
@@ -19,9 +19,10 @@ def download(s3client, bucket_name, filename , output_filename):
 
 start_time = time.time()
 
+print("starting download")
+
 download(s3client, bucket_name, filename , output_filename)
 
 end_time = time.time()
 
 print("download complete , time taken in seconds {} ".format(end_time - start_time))
-
